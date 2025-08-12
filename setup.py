766da89f9f9be6a7d1a68657a22128b66601a6b5
Rasika Deshmukh <rasikadeshmukh@Rasikas-MacBook-Pro-2.local>
2025-08-12 18:39:10 -0400

    Created folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Rasika Deshmukh',
+    author_email= 'rasikardeshmukh@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
